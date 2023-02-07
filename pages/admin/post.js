3634578315ec25a10e17b9e074bebd062f338e2b
avadhsakhiya <avadhsakhiya8@gmail.com>
2023-02-07 09:24:16 -0800

    redeploye

--- pages/admin/post.js
+++ pages/admin/post.js
@@ -1,10 +1,6 @@
-import axios from "axios"
+
 
 export default function Post() {
-
-  const getdata = (name,value) => {
-    console.log(name,value);
-  }
 
   return (
     <div>
@@ -15,33 +11,33 @@ export default function Post() {
         <form>
           <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Post title</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" name="postTitle" onInput={getdata(value)} />
+            <input type="text" class="form-control" id="exampleInputEmail1" name="postTitle" />
           </div>
           <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Page title</label>
-            <input type="text" class="form-control" id="exampleInputPassword1" name="pageTitle" onInput={()=>getdata(name,value)} />
+            <input type="text" class="form-control" id="exampleInputPassword1" name="pageTitle"  />
           </div>
           <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">meta description</label>
-            <input type="text" class="form-control" id="exampleInputPassword1" name="metaDescription" onInput={()=>getdata(name,value)} />
+            <input type="text" class="form-control" id="exampleInputPassword1" name="metaDescription"  />
           </div>
           <div class="mb-3">
             <label for="basic-url" class="form-label">URL structure</label>
             <div class="input-group">
               <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
-              <input type="date" class="form-control" id="basic-url" aria-describedby="basic-addon3" name="urlStructure" onInput={()=>getdata(name,value)} />
+              <input type="date" class="form-control" id="basic-url" aria-describedby="basic-addon3" name="urlStructure"  />
             </div>
           </div>
           <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Author name</label>
-            <input type="text" class="form-control" id="exampleInputPassword1" name="authorName" onInput={()=>getdata(name,value)} />
+            <input type="text" class="form-control" id="exampleInputPassword1" name="authorName"  />
           </div>
           <div class="input-group mb-3">
-            <input type="file" class="form-control" id="inputGroupFile02" name="imageUrl" onInput={()=>getdata(name,value)} />
+            <input type="file" class="form-control" id="inputGroupFile02" name="imageUrl" />
             <label class="input-group-text" for="inputGroupFile02">Upload Thumnail image</label>
           </div>
           <div class="mb-3">
-            <label for="inputState" class="form-label" name="Category" onInput={()=>getdata(name,value)}>Select Category</label>
+            <label for="inputState" class="form-label" name="Category" >Select Category</label>
             <select id="inputState" class="form-select">
               <option selected>Choose...</option>
               <option>...</option>
@@ -50,7 +46,7 @@ export default function Post() {
           <div class="mb-3">
             <label for="basic-url" class="form-label">Publish Date</label>
             <div class="input-group">
-              <input type="date" class="form-control" id="basic-url" aria-describedby="basic-addon3" name="publishDate" onInput={()=>getdata(name,value)} />
+              <input type="date" class="form-control" id="basic-url" aria-describedby="basic-addon3" name="publishDate" />
             </div>
           </div>
           <button type="submit" class="btn btn-primary">Add Post</button>
